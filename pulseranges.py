1790c12a8d2755590e5bb3cc8d64dd4bb3e6ef4a
Dominick Rowan <dmrowan@haverford.edu>
2019-04-20 12:08:55 -0400

    Command line options

--- pulseranges.py
+++ pulseranges.py
@@ -3,6 +3,11 @@ import argparse
 import numpy as np
 import LCClass
 
+desc="""
+Find primary and interpulse phase ranges with user
+defined offpeak phase ranges
+"""
+
 def find_phase_ranges(evt, off1, off2, nsigma):
     lc = LCClass.LightCurve(evt)
     lc.generate()
@@ -14,3 +19,15 @@ def find_phase_ranges(evt, off1, off2, nsigma):
     return cutofftup
 
 
+if __name__ == '__main__':
+    parser = arparse.ArgumentParser(description=desc)
+    parser.add_argument("--evt", help="Event file", type=str, 
+                        required=True)
+    parser.add_argument("--lower", help="Lower off peak phase", 
+                        type=float, required=True)
+    parser.add_argument("--upper", help="Upper off peak phase",
+                        type=float, required=True)
+    parser.add_argument("--sigma", help="Sigma limits for cutoff", 
+                        type=float, default=3.0)
+    args= parser.parse_args()
+    find_phase_range(args.evt, args.lower, args.upper, args.sigma)
