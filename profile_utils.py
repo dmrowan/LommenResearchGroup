7a85ac603fc8cbdeb46cc47a17254a5622d9cea6
Dominick Rowan <dmrowan@haverford.edu>
2019-05-27 22:09:55 -0400

    Added pulseranges

--- profile_utils.py
+++ profile_utils.py
@@ -11,6 +11,7 @@ desc="""
 Various profile tools to use for generating pulse profiles
 """
 
+# Produce energy profile over selected energy range in keV
 def energy_filtered_profile(evt, energy_min, energy_max, 
                             phase_min=None, phase_max=None):
     if type(evt) != str:
@@ -36,6 +37,7 @@ def energy_filtered_profile(evt, energy_min, energy_max,
         ax.axvspan(phase_min, phase_max, color='gray', alpha=.2)
     plt.show()
 
+#Generate profile and zoom on phase region
 def zoom_profile(evt, phase_min, phase_max):
 
     if type(evt) != str:
@@ -57,3 +59,25 @@ def zoom_profile(evt, phase_min, phase_max):
     ax.set_xlim(left=phase_min, right=phase_max)
     plt.subplots_adjust(bottom=.2, top=.98, right=.98, left=.15)
     plt.show()
+
+
+# Find primary and interpulse phase ranges with user
+# defined offpeak phase ranges
+def find_phase_ranges(evt, off1, off2, nsigma):
+
+    if type(evt) != str:
+        raise ValueError("filename must be string")
+    if any( [type(v) not in [float, int] for v in [off1, off2, nsigma] ]):
+        raise ValueError("value must be int or float")
+    if not os.path.isfile(evt):
+        raise FileNotFoundError
+
+    lc = LCClass.LightCurve(evt)
+    lc.generate()
+    cutofftup = lc.peak_cutoff(off1, off2, nsigma=nsigma)
+    print(f"Min phase primary: {cutofftup.min_phase}")
+    print(f"Max phase primary: {cutofftup.max_phase}")
+    print(f"Min phase interpulse: {cutofftup.min_phase_ip}")
+    print(f"Max phase interpulse: {cutofftup.max_phase_ip}")
+    return cutofftup
+
