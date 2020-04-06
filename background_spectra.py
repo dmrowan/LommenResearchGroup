01bdbd2b9d6fddde8855dead483acd7c7fd1c423
Dominick Rowan <dmrowan@haverford.edu>
2020-04-06 16:59:11 -0400

    Adjusted for new niutils and with new plotting function

--- background_spectra.py
+++ background_spectra.py
@@ -26,26 +26,33 @@ XSPEC analysis of BKGD_RXTE data from NICER
 
 rc('text', usetex=True)
 
+#Generate a text data file from spectra using xspec
 def xspec_routine(pha):
     assert(os.path.isfile(pha))
 
     datafile = pha.replace('.pha', '_data.txt')
 
+    #Start xspec child
     xspec = pexpect.spawn("xspec")
     xspec.expect('XSPEC12>')
 
+    #Load data
     xspec.sendline(f'data 1 {pha}')
     xspec.expect('XSPEC12>')
 
+    #Set plotting device
     xspec.sendline('cpd /xs')
     xspec.expect('XSPEC12>')
 
+    #Choose energy range
     xspec.sendline('ig **-.2, 10.-**')
     xspec.expect('XSPEC12>')
 
+    #Change axis
     xspec.sendline('setplot energy')
     xspec.expect('XSPEC12>')
 
+    #Plot in xs
     xspec.sendline('plot data')
     xspec.expect('XSPEC12>')
 
@@ -54,6 +61,7 @@ def xspec_routine(pha):
     xspec.sendline('ipl')
     xspec.expect('PLT>')
 
+    #Save txt file
     xspec.sendline(f"wdata {datafile}")
     if os.path.isfile(datafile):
         xspec.expect('exists, reuse it?')
@@ -66,7 +74,7 @@ def xspec_routine(pha):
 def xspec_environ_spectra(bkg):
     assert(os.path.isfile(bkg))
 
-    datafile = bkg.replace('_bkg.pha', '_bkg_data.txt')
+    datafile = bkg.replace('.pha', '_data.txt')
 
     xspec = pexpect.spawn("xspec")
     xspec.expect('XSPEC12>')
@@ -89,124 +97,131 @@ def xspec_environ_spectra(bkg):
     xspec.sendline('exit')
 
 
-def plot_bkgd_with_environmental(data, bkg):
-    fig, ax = plt.subplots(1, 1, figsize=(12, 6))
-    ax = niutils.plotparams(ax)
-    colors = ["#9749b9", "#669c55", "#b54958", "#757cad", "#bb8140"]
-    colors_bkg = ['gray']*len(colors)
+def reformat_input(l):
+    for i in range(len(l)):
+        if type(l[i]) == str:
+            l[i] = [l[i]]
+    return l
 
-    df = pd.read_csv(data, skiprows=3, delimiter=" ", header=None)
-    df.columns = ['energy', 'energy_err', 'counts', 'counts_err']
+def check_length(l):
+    l = reformat_input(l)
+    return len(l), [ len(elem) for elem in l ]
 
-    df_bkg = pd.read_csv(bkg, skiprows=3, delimiter=" ", header=None)
-    df_bkg.columns = ['energy', 'energy_err', 'counts', 'counts_err']
+def plot_bkgd_spectra(br_data_list, br_ranges,
+                      sa_ranges=None,
+                      environ=None, bkg3c50=None,
+                      two_panel=False, savefig=None,
+                      zoom_top=0.3, zoom_bottom=0.9,
+                      zoom_left=0.3, zoom_right=0.9):
 
-    ax.errorbar(df['energy'], df['counts'], 
-                xerr=df['energy_err'], yerr=df['counts_err'],
-                ls=' ', marker='.', color=colors[2], 
-                label='60--80')
+    assert(len(br_data_list) == len(br_ranges))
 
-    ax.errorbar(df_bkg['energy'], df_bkg['counts'], 
-                xerr=df_bkg['energy_err'], yerr=df_bkg['counts_err'],
-                ls=' ', marker='.', color=colors_bkg[2], 
-                label='BKG Model')
+    br_data_list = reformat_input(br_data_list)
 
-    ax.legend(edgecolor='black', fontsize=20)
-    ax.set_ylabel(r'Normalized Counts (s$^{-1}$ keV$^{-1}$)', fontsize=20)
-    ax.set_xlabel('Energy (keV)', fontsize=20)
+    if any( [ l > 1 for l in check_length(br_data_list)[1] ] ):
+        assert(sa_ranges is not None)
 
-    fig.savefig("first_environmental_attempt.pdf")
+        sa_ranges = reformat_input(sa_ranges)
+        assert(check_length(br_data_list) == check_length(sa_ranges))
 
-def plot_all_environmental(bkg_list, labels):
-    fig, ax = plt.subplots(1, 1, figsize=(12,6))
-    ax = niutils.plotparams(ax)
 
-    for bkg, l in zip(bkg_list, labels):
-        df_bkg = pd.read_csv(bkg, skiprows=3, delimiter=" ", header=None)
-        df_bkg.columns = ['energy', 'energy_err', 'counts', 'counts_err']
 
-        ax.errorbar(df_bkg['energy'], df_bkg['counts'], 
-                    xerr=df_bkg['energy_err'], yerr=df_bkg['counts_err'],
-                    ls=' ', marker='.',
-                    label=l)
+    linestyles = ['-', '--', '-.', ':']
 
-    ax.legend(edgecolor='black', fontsize=20)
-    ax.set_ylabel(r'Normalized Counts (s$^{-1}$ keV$^{-1}$)', fontsize=20)
-    ax.set_xlabel('Energy (keV)', fontsize=20)
+    data_flat = []
+    br_ranges_flat = []
+    sa_ranges_flat = []
+    ls_flat = []
 
-    plt.show()
+    for i in range(len(br_data_list)):
+        for j in range(len(br_data_list[i])):
+            data_flat.append(br_data_list[i][j])
+            br_ranges_flat.append(br_ranges[i])
+            if sa_ranges is not None:
+                sa_ranges_flat.append(sa_ranges[i][j])
+            ls_flat.append(linestyles[j])
 
-def plot_bkgd_spectra(data_list, ranges, environ=None, bkg3c50=None):
-    fig, (ax, ax1) = plt.subplots(2, 1, figsize=(12, 12))
-    plt.subplots_adjust(hspace=.1)
-    ax = niutils.plotparams(ax)
-    ax1 = niutils.plotparams(ax1)
-
-    colors = niutils.get_colors()['br_colors']
+    colors_flat = [ niutils.map_colors()[r] for r in br_ranges_flat ]
+    if sa_ranges is None:
+        labels_flat = [ niutils.br_label(r) for r in br_ranges_flat ]
+    else:
+        labels_flat = [ f'{niutils.br_label(br_ranges_flat[i])}; {niutils.sa_label(sa_ranges_flat[i])}' for i in range(len(br_ranges_flat)) ]
 
     if environ is not None:
-        data_list.append(environ)
-        colors.append(niutils.get_colors()['environ_color'])
-        ranges.append("Environmental Model")
+        data_flat.append(environ)
+        colors_flat.append(niutils.get_colors()['environ_color'])
+        labels_flat.append("Environmental Model")
+        ls_flat.append('-')
 
     if bkg3c50 is not None:
-        data_list.append(bkg3c50)
-        colors.append(niutils.get_colors()['3c50_color'])
-        ranges.append("3C50 Model")
+        data_flat.append(bkg3c50)
+        colors_flat.append(niutils.get_colors()['3c50_color'])
+        labels_flat.append('3C50 Model')
+        ls_flat.append('-')
 
+    if two_panel:
+        fig, (ax, ax1) = plt.subplots(2, 1, figsize=(16, 16))
+        plt.subplots_adjust(hspace=.1)
+        ax_list = [ax, ax1]
 
-    for i in range(len(data_list)):
-        df = pd.read_csv(data_list[i], skiprows=3, delimiter=" ", header=None)
+    else:
+        fig, ax = plt.subplots(1, 1, figsize=(15, 8))
+        ax_list = [ax]
+
+    for a in ax_list: a = niutils.plotparams(a)
+
+    for i in range(len(data_flat)):
+        df = pd.read_csv(data_flat[i], skiprows=3, delimiter=" ", header=None)
         df.columns = ['energy', 'energy_err', 'counts', 'counts_err']
 
-        ax.errorbar(df['energy'], df['counts'], 
-                    xerr=df['energy_err'], yerr=df['counts_err'],
-                    ls=' ', marker='.', color=colors[i], 
-                    label=ranges[i])
-
-        ax1.errorbar(df['energy'], df['counts'], 
-                    xerr=df['energy_err'], yerr=df['counts_err'],
-                    ls=' ', marker='.', color=colors[i], 
-                    label=ranges[i])
-
+        for a in ax_list:
+            a.errorbar(df['energy'], df['counts'],
+                       xerr=df['energy_err'], yerr=df['counts_err'],
+                       ls=ls_flat[i], marker='.', color=colors_flat[i],
+                       label=labels_flat[i])
 
     ax.legend(edgecolor='black', fontsize=20)
 
-    for a in [ax,ax1]:
+    for a in ax_list:
         a.set_ylabel(r'Normalized Counts (s$^{-1}$ keV$^{-1}$)', fontsize=20)
         a.set_xscale('log')
         a.set_yscale('log')
+    ax_list[-1].set_xlabel('Energy (keV)', fontsize=20)
     ax.xaxis.set_major_formatter(mticker.FormatStrFormatter('%.1f'))
-    ax1.xaxis.set_minor_formatter(mticker.FormatStrFormatter('%.01f'))
+    if two_panel:
+        ax1.xaxis.set_minor_formatter(mticker.FormatStrFormatter('%.01f'))
+        ax1.set_xlim(zoom_bottom, zoom_top)
+        ax1.set_ylim(zoom_left, zoom_right)
 
-    ax1.set_xlabel('Energy (keV)', fontsize=20)
+        con1 = ConnectionPatch(xyA=(ax1.get_xlim()[0], ax1.get_ylim()[1]),
+                               xyB=(ax1.get_xlim()[0], ax1.get_ylim()[0]),
+                               coordsA='data', coordsB='data',
+                               axesA=ax1, axesB=ax,
+                               color='black', ls='-')
 
-    ax1.set_xlim(left=.28, right=.82)
-    ax1.set_ylim(top=.92, bottom=.3)
-
-    con1 = ConnectionPatch(xyA=(ax1.get_xlim()[0], ax1.get_ylim()[1]),
-                           xyB=(ax1.get_xlim()[0], ax1.get_ylim()[0]),
-                           coordsA='data', coordsB='data',
-                           axesA=ax1, axesB=ax, 
-                           color='black', ls='-')
-
-    con2 = ConnectionPatch(xyA=(ax1.get_xlim()[1], ax1.get_ylim()[1]),
-                           xyB=(ax1.get_xlim()[1], ax1.get_ylim()[0]),
-                           coordsA='data', coordsB='data',
-                           axesA=ax1, axesB=ax,
-                           color='black', ls='-')
+        con2 = ConnectionPatch(xyA=(ax1.get_xlim()[1], ax1.get_ylim()[1]),
+                               xyB=(ax1.get_xlim()[1], ax1.get_ylim()[0]),
+                               coordsA='data', coordsB='data',
+                               axesA=ax1, axesB=ax,
+                               color='black', ls='-')
 
 
-    rect = Rectangle((ax1.get_xlim()[0], ax1.get_ylim()[0]), 
-                     (ax1.get_xlim()[1]-ax1.get_xlim()[0]),
-                     (ax1.get_ylim()[1]-ax1.get_ylim()[0]), 
-                     facecolor='none', edgecolor='black', 
-                     alpha=.5)
-    ax.add_patch(rect)
-    ax1.add_patch(con1)
-    ax1.add_patch(con2)
+        rect = Rectangle((ax1.get_xlim()[0], ax1.get_ylim()[0]),
+                         (ax1.get_xlim()[1]-ax1.get_xlim()[0]),
+                         (ax1.get_ylim()[1]-ax1.get_ylim()[0]),
+                         facecolor='none', edgecolor='black',
+                         alpha=.5)
+        ax.add_patch(rect)
+        ax1.add_patch(con1)
+        ax1.add_patch(con2)
 
-    fig.savefig("BKGD_spectra.pdf")
+    if savefig is not None:
+        fig.savefig(savefig)
+        if savefig.endswith('pdf'):
+            fig.savefig(savefig.replace('pdf', 'png'))
+
+    else:
+        plt.show()
 
 
 def make_spectra(): 
@@ -220,25 +235,14 @@ def make_spectra():
                                     save_pha=evtlist[i].replace('evt', 'pha')) 
 
 if __name__ == '__main__':
-    pha_list = ['br_earth_--40.pha', 'br_earth_40--60.pha', 
-                'br_earth_60--80.pha',
-                'br_earth_80--180.pha', 'br_earth_180--.pha']
-    ranges = ['0--40', '40--60', '60--80', '80--180', '180--200']
-    """
-    make_spectra()
 
-    pha_list = ['br_earth_--40.pha', 'br_earth_40--60.pha', 
-                'br_earth_60--80.pha',
-                'br_earth_80--180.pha', 'br_earth_180--.pha']
-
-    ranges = ['0--40', '40--60', '60--80', '80--180', '180--200']
-    for pha in pha_list:
-        xspec_routine(pha)
-    """
-    plot_bkgd_spectra([pha.replace('.pha', '_data.txt') for pha in pha_list ],
-                      ranges, 
-                      environ='environ_all/data_environ_all.txt', 
-                      bkg3c50='3c50/data_3c50.txt')
-
-    #plot_bkgd_with_environmental('br_earth_60--80_data.txt', 'br_earth_60--80_bkg_data.txt')
+    plot_bkgd_spectra(['br_earth_--40_data.txt',
+                       'br_earth_40--60_data.txt', 
+                       'br_earth_60--80_data.txt', 
+                       'br_earth_80--180_data.txt', 
+                       'br_earth_180--_data.txt'], 
+                       ['--40', '40--60', '60--80', '80--180', '180--'],
+                       environ='environ_all/data_environ_all.txt',
+                       bkg3c50='3c50/data_3c50.txt',
+                       savefig='BKGD_spectra.pdf')
 
