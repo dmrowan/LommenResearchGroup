104400e0c1c3eedc8e6c5ae3bfcea33567a0f052
Ruhl Nathaniel <nruhl@anita>
2020-06-12 17:55:22 -0400

    Addition of an Energy Band class

--- HorizonCrossing/elevation_nate.py
+++ HorizonCrossing/elevation_nate.py
@@ -3,17 +3,15 @@ import numpy as np
 from astropy.table import Table
 import matplotlib.pyplot as plt
 from scipy import interpolate
-#s = np.where(elevArray==min(elevArray))
-#e = np.where(elevArray==max(elevArray))
 
-########################################################
+#Time range around the horizon crossing
 startTime = 390+1.92224*10**8
 stopTime = 500+1.92224*10**8
 
 startTimeIndex = 311883
 stopTimeIndex = 352360
 
-########################################################
+#read in the data files
 
 tab_ni = Table.read('ni2200300102.mkf',hdu=1)
 timeArray = np.array(tab_ni['TIME'])
@@ -25,16 +23,17 @@ tab_evt = Table.read('cleanfilt.evt',hdu=1)
 eventTime = np.array(tab_evt['TIME'][startTimeIndex:stopTimeIndex])
 enArray = np.array(tab_evt['PI'][startTimeIndex:stopTimeIndex])
 
+#bin size and energy band cutoffs
+binSize_all = 1
+lowEn = [30,100]
+midEn = [100,200]
+highEn = [200,1000]
 
-########################################################
-#interpolation for the elevation values from mkf, applied to evt times
-
+#interpolate the times.evt to go over the range of elevations.mkf
 f = interpolate.interp1d(timeArray,elevArray,kind='linear')
-
 elev_evt = f(eventTime)
 
-
-##########################################################
+#calculate altitude based on elevation angle
 R = 6378
 H = 410
 theta = np.arcsin(R/(R+H))
@@ -43,16 +42,15 @@ for val in elev_evt:
   altArray.append((R+H)*np.sin(theta+val*(np.pi/180))-R)
 altArray=np.array(altArray)
 
+#function that splits the altitudes based on energy bands
 def altSplit(energy_level):
   index=np.where((enArray>=energy_level[0])&(enArray<energy_level[1]))
   return altArray[index[0]]
-##########################################################
+
 #function that deduces the number of counts per bin size 
 def countRate(Time,binSize):
-  mintime=min(Time)
-  maxtime=max(Time)
   binCounts=[]
-  for i in np.arange(mintime,maxtime+binSize,binSize):
+  for i in np.arange(min(Time),max(Time)+binSize,binSize):
     desind=np.where((Time >= i) & (Time < i + binSize))
     binCounts.append(np.size(desind[0]))
   return np.array(binCounts)
@@ -62,72 +60,37 @@ def enSplit(energy_level):
   index=np.where((enArray>=energy_level[0])&(enArray<energy_level[1]))
   return eventTime[index[0]]
 
-#bin size and energy cutoffs
-binSize_all = 1
-lowEn = [30,100]
-midEn = [100,200]
-highEn = [200,1000]
+#time axis from count rate vs time plot
+def createAxis(alt_split,bin_size):
+  return np.arange(0,len(alt_split),bin_size)
 
-
-
-#times at which each energy occurs
-lowTime = enSplit(lowEn)
-midTime = enSplit(midEn)
-highTime = enSplit(highEn)
-
-
-#number of counts in each bin of time (counts per second)
-lowBinRate = countRate(lowTime,binSize_all)
-midBinRate = countRate(midTime,binSize_all)
-highBinRate = countRate(highTime,binSize_all)
-
-#splits the elevation values into three separaate arrays for each energy band
-lowEnergyAlt = altSplit(lowEn)
-midEnergyAlt = altSplit(midEn)
-highEnergyAlt = altSplit(highEn)
-###############################################################################
-#interpolate the to cover the range of elevation
-lowaxis=np.arange(0,len(lowEnergyAlt),binSize_all)
-midaxis=np.arange(0,len(midEnergyAlt),binSize_all)
-highaxis=np.arange(0,len(highEnergyAlt),binSize_all)
-
-
+#interpolate the count rate to match the range of elevations
 def newAlt(Axis,oldAlt,CountRate):
   g = interpolate.interp1d(Axis,oldAlt,kind='linear')
   return g(np.arange(0,len(CountRate),binSize_all))
 
-new_lowEnergyAlt = newAlt(lowaxis,lowEnergyAlt,lowBinRate)
-new_midEnergyAlt = newAlt(midaxis,midEnergyAlt,midBinRate)
-new_highEnergyAlt = newAlt(highaxis,highEnergyAlt,highBinRate)
+class EnergyBands:
 
-print(len(new_lowEnergyAlt))
-print(len(lowBinRate))
+  def __init__(self,energy_band,bin_size):
+    self.energy_band = energy_band
+    self.bin_size = bin_size
+    self.time = enSplit(energy_band)
+    self.rate = countRate(self.time,bin_size)
+    self.alt = altSplit(energy_band)
+    self.axis = createAxis(self.alt,bin_size)
+    self.new_alt = newAlt(self.axis,self.alt,self.rate)
 
-print(len(new_midEnergyAlt))
-print(len(midBinRate))
 
-print(len(new_highEnergyAlt))
-print(len(highBinRate))
+low_en = EnergyBands(lowEn,binSize_all)
+mid_en = EnergyBands(midEn,binSize_all)
+high_en = EnergyBands(highEn,binSize_all)
 
 ################################################################################
-'''
-#indices corresponding to start and stop of crossing -- I used np.where(timeArray==starttime+Xint), np.where(timeArray==starttime+Ratemax)
-print(elevArray[2265])
-print(elevArray[2365])
-print(altArray[2265])
-print(altArray[2365])
+#plot the data
 
-plt.plot(altArray[2265:2365],enArray_low[2265:2365],'b.')
-plt.plot(altArray[2265:2365],enArray_mid[2265:2365],'r.')
-plt.title('NICER Elevation During Feb 3 Crossing')
-plt.xlabel('Altitude of X-Ray Transparency (km)')
-plt.ylabel('Count Rate')
-plt.show()
-##########################################################
-'''
-plt.plot(new_lowEnergyAlt,lowBinRate,'r.',label='.3-1 KeV')
-plt.plot(new_midEnergyAlt,midBinRate,'g.',label='1-2 KeV')
-plt.plot(new_highEnergyAlt,highBinRate,'b.',label='2-10 KeV')
+plt.plot(low_en.new_alt,low_en.rate,'r.',label=f'{lowEn[0]/100}keV-{lowEn[1]/100}keV')
+plt.plot(mid_en.new_alt,mid_en.rate,'g.',label=f'{midEn[0]/100}keV-{midEn[1]/100}keV')
+plt.plot(high_en.new_alt,high_en.rate,'b.',label=f'{highEn[0]/100}keV-{highEn[1]/100}keV')
 plt.title("Counts/Sec vs. Altitude for 3 Energy Bands (FEB 3)")
 plt.xlabel("Altitude (km)")
 plt.ylabel("X-Ray Photon Counts/Sec")
