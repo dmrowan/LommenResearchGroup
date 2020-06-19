c539ebd7560a63e3eb503ae7493788e77ab9c2f4
Ruhl Nathaniel <nruhl@anita>
2020-06-19 11:21:25 -0400

    new function module

--- HorizonCrossing/elevation_nate.py
+++ HorizonCrossing/elevation_nate.py
@@ -1,173 +1,19 @@
 #!/usr/bin/env python
-import numpy as np
-import math
-from astropy.table import Table
 import matplotlib.pyplot as plt
-import scipy.signal
-from scipy import interpolate
-from scipy.optimize import curve_fit
-from scipy import signal
-from scipy.integrate import quad
-
-# Time range around the horizon crossing
-startTime = 390+1.92224*10**8
-stopTime = 500+1.92224*10**8
-
-startTimeIndex = 311883
-stopTimeIndex = 352360
-
-# read in the data files
-
-tab_ni = Table.read('ni2200300102.mkf', hdu=1)
-timeArray = np.array(tab_ni['TIME'])
-elevArray = np.array(tab_ni['ELV'])
-enArray_low = np.array(tab_ni['FPM_XRAY_PI_0035_0200'])
-enArray_mid = np.array(tab_ni['FPM_XRAY_PI_0800_1200'])
-
-# print(elevArray)
-# print(tab_ni['ATT_ANG_EL'])
-# print(tab_ni['ATT_ANG_AZ'])
-
-tab_evt = Table.read('cleanfilt.evt', hdu=1)
-eventTime = np.array(tab_evt['TIME'][startTimeIndex:stopTimeIndex])
-enArray = np.array(tab_evt['PI'][startTimeIndex:stopTimeIndex])
+import functions_mod as f
 
 
-# bin size and energy band cutoffs
-binSize_all = 1
-lowEn = [0, 125]
-midEn = [125, 200]
-highEn = [200, 600]
-allEn = [0, 600]
+low_en = f.EnergyBands(f.lowEn, f.binSize_all)
+mid_en = f.EnergyBands(f.midEn, f.binSize_all)
+high_en = f.EnergyBands(f.highEn, f.binSize_all)
+all_en = f.EnergyBands(f.allEn, f.binSize_all)
 
 
-# interpolate the times.evt to go over the range of elevations.mkf
-f = interpolate.interp1d(timeArray, elevArray, kind='linear')
-elev_evt = f(eventTime)
-
-
-# calculate altitude based on elevation angle
-R = 6378
-H = 410
-theta = np.arcsin(R/(R+H))
-altArray = []
-for val in elev_evt:
-    h = ((R+H)*np.sin(theta+val*(np.pi/180)))-R
-    altArray.append(h)
-altArray = np.array(altArray)
-
-
-# function that splits the altitudes based on energy bands
-def altSplit(energy_level):
-    index = np.where((enArray >= energy_level[0]) & (enArray < energy_level[1]))
-    return altArray[index[0]]
-
-
-# function that deduces the number of counts per bin size
-def countRate(Time, alt_array, binSize):
-    binCounts = []
-    altitude = []
-    for i in np.arange(min(Time), max(Time)+binSize, binSize):
-        desind = np.where((Time >= i) & (Time < i + binSize))
-        if len(alt_array[desind[0]]) != 0:
-            binCounts.append(np.size(desind[0]))
-            altitude.append(np.mean(alt_array[desind[0]]))
-    return np.array(binCounts), np.array(altitude)
-
-
-# function that makes a list of times corresponding to each energy range
-def enSplit(energy_level):
-    index = np.where((enArray >= energy_level[0]) & (enArray < energy_level[1]))
-    return eventTime[index[0]], enArray[index[0]]
-
-
-def percTrans(Alt, Rate):
-    plateau = np.where(((Alt > 200) & (Alt < 250)))
-    avg = np.mean(Rate[plateau[0]])
-    return (Rate/avg)*100
-
-
-def SeventhOr(x, a, b, c, d, e, f, g, h):
-    return(a*x**7+b*x**6+c*x**5+d*x**4+e*x**3+f*x**2+g*x+h)
-
-
-def curveFit(newAlt, Rate):
-    popt, pcov = curve_fit(SeventhOr, newAlt, Rate)
-    return popt, pcov
-
-
-# functions to make the atmospheric model
-# altArray=h in mathematica
-L = 8.5
-z0 = 80
-rho0 = 0.0012*np.exp(-z0/L)
-
-
-def Dtot(Alt):
-    return 2*np.sqrt((R+H)**2-(R+Alt)**2)
-
-
-def Sigma(energy):
-    c = np.float(-3)
-    return 3.38*(np.mean(energy)/100)**c
-
-
-# i is the index in altArray
-def Z(x, i, Alt):
-    return np.sqrt(x**2+(R+Alt[i])**2)-R
-
-
-def Rho(x, i, Alt):
-    return rho0*np.exp(-(Z(x, i, Alt)-z0)/L)
-
-
-def Transmit(Alt, sigma, dist):
-    elem = 100
-    tau = []
-    for hi in range(len(Alt)):
-        g = 0
-        x2 = (dist[hi]*10**5)/2
-        X = np.linspace(0, x2, elem)
-        for n in X:
-            dx = x2/elem
-            g += Rho(n, hi, Alt)*dx
-        tau.append(-2*sigma*g)
-    tau = np.array(tau)
-    trans = 100*np.exp(tau)
-    return trans
-
-
-class EnergyBands:
-
-    def __init__(self, energy_band, bin_size):
-        self.energy_band = energy_band
-        self.bin_size = bin_size
-        self.time, self.energies = enSplit(energy_band)
-        self.sigmaN = Sigma(self.energies)
-        self.alt = altSplit(energy_band)
-        self.rate, self.new_alt = countRate(self.time, self.alt, bin_size)
-        self.perc_trans = percTrans(self.new_alt, self.rate)
-        self.dtot = Dtot(self.alt)
-        self.trans_model = Transmit(self.alt, self.sigmaN, self.dtot)
-        self.popt, self.pcov = curveFit(self.new_alt, self.rate)
-        self.popt_perc, self.pcov_perc = curveFit(self.new_alt, self.perc_trans)
-
-
-low_en = EnergyBands(lowEn, binSize_all)
-mid_en = EnergyBands(midEn, binSize_all)
-high_en = EnergyBands(highEn, binSize_all)
-all_en = EnergyBands(allEn, binSize_all)
-
-print(len(low_en.alt))
-print(len(mid_en.alt))
-print(len(high_en.alt))
-tot_length = len(low_en.alt) + len(mid_en.alt) + len(high_en.alt)
-print(f'Does {tot_length} = {len(all_en.alt)}?')
-
-plt.plot(low_en.alt, low_en.trans_model, 'r--', markersize='5', label=f'{lowEn[0]/100}keV-{lowEn[1]/100}keV')
-plt.plot(mid_en.alt, mid_en.trans_model, 'g--', markersize='5', label=f'{midEn[0]/100}keV-{midEn[1]/100}keV')
-plt.plot(high_en.alt, high_en.trans_model, 'b--', markersize='5', label=f'{highEn[0]/100}keV-{highEn[1]/100}keV')
-plt.title('Percent Transmission vs Altitude -- Local Exponential Model')
+plt.plot(low_en.alt, low_en.trans_model, 'r.', markersize='5', label=f'{f.lowEn[0]/100}keV-{f.lowEn[1]/100}keV (expected)')
+plt.plot(low_en.new_alt, low_en.perc_trans, 'b.', label=f'{f.lowEn[0]/100}keV-{f.lowEn[1]/100}keV (measured)')
+# plt.plot(mid_en.alt, mid_en.trans_model, 'g.', markersize='5', label=f'{midEn[0]/100}keV-{midEn[1]/100}keV')
+# plt.plot(high_en.alt, high_en.trans_model, 'b.', markersize='5', label=f'{highEn[0]/100}keV-{highEn[1]/100}keV')
+plt.title(f'Percent Transmission vs Altitude (Scale Height ={f.L}km)')
 plt.ylabel('Percent Transmission (%)')
 plt.xlabel('Tangential Altitude')
 plt.grid()
