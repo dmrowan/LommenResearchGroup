17c8616650d34105c572a2963bbc45f7b889d7e1
Schwab Noah <nhschwab@anita>
2020-06-26 12:05:00 -0400

    Paramater Update

--- HorizonCrossing/atmospheric_model_nate.py
+++ HorizonCrossing/atmospheric_model_nate.py
@@ -14,9 +14,9 @@ plt.figure(1)
 plt.plot(low_en.alt, low_en.trans_model, 'k-', markersize='5', label=f'{f.lowEn[0]/100}keV-{f.lowEn[1]/100}keV (expected model)')
 plt.plot(low_en.new_alt, low_en.perc_trans, 'r.', label=f'{f.lowEn[0]/100}keV-{f.lowEn[1]/100}keV (data)')
 
-popt, pcov = curve_fit(f.Transmit, low_en.new_alt, low_en.perc_trans, bounds=([5,0,10],[10,10**-4,25]))
+popt, pcov = curve_fit(lambda Alt, p0, l: f.Transmit(Alt, low_en.sigmaN, p0, l), low_en.new_alt, low_en.perc_trans)
 
-plt.plot(low_en.alt, f.Transmit(low_en.alt, *popt), 'k--', label='data fit to model')
+plt.plot(low_en.alt, f.Transmit(low_en.alt, low_en.sigmaN, *popt), 'k--', label='data fit to model')
 
 plt.title('Percent Transmission vs Altitude')
 plt.ylabel('Percent Transmission (%)')
@@ -29,13 +29,13 @@ print("low energy parameters")
 print(f"Mean energy: {np.mean(low_en.energies/100)}")
 
 print("Expected sigmaN: ", low_en.sigmaN)
-print("Measured sigmaN: ", popt[0])
+# print("Measured sigmaN: ", popt[0])
 
 print("Expected rho0: ", f.rho0)
-print("Measured rho0:", popt[1])
+print("Measured rho0:", popt[0])
 
 print("Expected L: ", f.L)
-print("Measured L: ", popt[2])
+print("Measured L: ", popt[1])
 
 print('----------------------')
 
@@ -43,9 +43,9 @@ plt.figure(2)
 plt.plot(mid_en.alt, mid_en.trans_model, 'k-', markersize='5', label=f'{f.midEn[0]/100}keV-{f.midEn[1]/100}keV (expected model)')
 plt.plot(mid_en.new_alt, mid_en.perc_trans, 'g.', label=f'{f.midEn[0]/100}keV-{f.midEn[1]/100}keV (data)')
 
-popt, pcov = curve_fit(f.Transmit, mid_en.new_alt, mid_en.perc_trans, bounds=([0,0,10],[4,10**-4,25]))
+popt, pcov = curve_fit(lambda Alt, p0, l: f.Transmit(Alt, mid_en.sigmaN, p0, l), mid_en.new_alt, mid_en.perc_trans)
 
-plt.plot(mid_en.alt, f.Transmit(mid_en.alt, *popt), 'k--', label='data fit to model')
+plt.plot(mid_en.alt, f.Transmit(mid_en.alt, mid_en.sigmaN, *popt), 'k--', label='data fit to model')
 
 plt.title('Percent Transmission vs Altitude')
 plt.ylabel('Percent Transmission (%)')
@@ -58,13 +58,13 @@ print("mid energy parameters")
 print(f"Mean energy: {np.mean(mid_en.energies/100)}")
 
 print("Expected sigmaN: ", mid_en.sigmaN)
-print("Measured sigmaN: ", popt[0])
+#print("Measured sigmaN: ", popt[0])
 
 print("Expected rho0: ", f.rho0)
-print("Measured rho0:", popt[1])
+print("Measured rho0:", popt[0])
 
 print("Expected L: ", f.L)
-print("Measured L: ", popt[2])
+print("Measured L: ", popt[1])
 
 print('----------------------')
 
@@ -72,9 +72,9 @@ plt.figure(3)
 plt.plot(high_en.alt, high_en.trans_model, 'k-', markersize='5', label=f'{f.highEn[0]/100}keV-{f.highEn[1]/100}keV (expected model)')
 plt.plot(high_en.new_alt, high_en.perc_trans, 'b.', label=f'{f.highEn[0]/100}keV-{f.highEn[1]/100}keV (data)')
 
-popt, pcov = curve_fit(f.Transmit, high_en.new_alt, high_en.perc_trans, bounds=([0,0,0],[1,10**-3,25]))
+popt, pcov = curve_fit(lambda Alt, p0, l: f.Transmit(Alt, high_en.sigmaN, p0, l), high_en.new_alt, high_en.perc_trans)
 
-plt.plot(high_en.alt, f.Transmit(high_en.alt, *popt), 'k--', label='data fit to model')
+plt.plot(high_en.alt, f.Transmit(high_en.alt, high_en.sigmaN, *popt), 'k--', label='data fit to model')
 
 plt.title('Percent Transmission vs Altitude')
 plt.ylabel('Percent Transmission (%)')
@@ -87,12 +87,12 @@ print("high energy parameters")
 print(f"Mean energy: {np.mean(high_en.energies/100)}")
 
 print("Expected sigmaN: ", high_en.sigmaN)
-print("Measured sigmaN: ", popt[0])
+#print("Measured sigmaN: ", popt[0])
 
 print("Expected rho0: ", f.rho0)
-print("Measured rho0:", popt[1])
+print("Measured rho0:", popt[0])
 
 print("Expected L: ", f.L)
-print("Measured L: ", popt[2])
+print("Measured L: ", popt[1])
 
 plt.show()
